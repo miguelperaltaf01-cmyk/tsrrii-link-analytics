0637f0efda0ef51dd7e89b6a0275d732c60ffd40
miguelperaltaf01-cmyk <miguelperaltaf01@gmail.com>
2026-08-23 02:26:49 -0300

    Add slug redirect endpoint

---
+++ api/redirect.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  const { slug } = req.query;
+
+  if (!slug) {
+    return res.status(400).send("Falta el slug");
+  }
+
+  try {
+    const response = await fetch(
+      `${process.env.SUPABASE_URL}/rest/v1/links?slug=eq.${encodeURIComponent(slug)}&select=id,slug,destination_url`
+    );
+
+    if (!response.ok) {
+      const error = await response.text();
+      return res.status(response.status).send(error);
+    }
+
+    const links = await response.json();
+
+    if (!links.length) {
+      return res.status(404).send("Enlace no encontrado");
+    }
+
+    const link = links[0];
+
+    return res.redirect(302, link.destination_url);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
